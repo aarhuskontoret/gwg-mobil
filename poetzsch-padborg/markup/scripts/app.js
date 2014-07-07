6db0f1b69ab1304cdc4b24eb7a9693b69932e46c
danielwi <mail@danielw.net>
2014-07-07 11:49:26 +0200

    CSS og JS opdateringer
    Opdateret CSS på alle sites
    rettet JS på Poetzsch

--- poetzsch-padborg/markup/scripts/app.js
+++ poetzsch-padborg/markup/scripts/app.js
@@ -2,43 +2,6 @@ var base = (function() {
 
 	return {
 
-		loadShops: function() {
-			var markers = [];
-			var container = $('#shops');
-			var shopsList = '<ul>';
-
-			$.getJSON('butikker.json', function(feed) {
-				$.each(feed,function(key,data) {
-					
-					// MARKERS ON MAP
-					var infoContentString = '<div class="shop-info">'+
-						//'<img src="'+data.logo+'" />'+ //logo
-						'<p>'+data.adress[0]+'<br>'+data.adress[1]+'<br>'+data.adress[2]+'</p>'+
-						'<h4>Åbningstider</h4>';
-					for(var i in data.open) {
-						infoContentString += '<strong>'+i+'</strong><br>'+
-						data.open[i]+'<br>';
-					}
-						infoContentString += "<div class='links'>"+
-							"<a href='http://www.google.dk/maps/dir//"+data.latlng[0]+","+data.latlng[1]+"/' target='_blank'>Se rutevejledning</a></div>";
-
-					shopsList += '<li><a href="#" class="title">'+data.name+'</a>'+
-					infoContentString+
-					'</li>';
-				});
-
-				shopsList += "</ul>";
-				container.append(shopsList);
-			});
-
-			container.on('click','.title',function() {
-				var shop = $(this).parent('li');
-				shop.toggleClass('active');
-				$('.shop-info',shop).slideToggle();
-
-				return false;
-			});
-		},
 		init: function() {
 			var scope = this;
 			if($('.flexslider').length) {
@@ -47,10 +10,6 @@ var base = (function() {
 					directionNav: false,
 					animation: 'slide'
 				});
-			}
-
-			if($('#shops').length){
-				scope.loadShops();
 			}
 
 			$('#menuTrigger').click(function(e) {
@@ -67,3 +26,21 @@ var base = (function() {
 $(document).ready(function() {
 	base.init();
 });
+
+var toldSlideHandler = (function ($, window, document) {  
+
+	this.slideContent = function (relId) {
+		var pDir = parseInt(relId,10);
+		$('#toldSkema').animate({ left:0 - (pDir*100)+"%"}, 400);
+		$('#toldreglerTop ul li a').each(function(){
+
+			if($(this).attr('rel') == relId ){
+				$(this).addClass('active');
+			} else {
+				$(this).removeClass('active');
+			}
+		});
+    };
+    return this;
+
+}(jQuery, window, document));
